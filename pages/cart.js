a7f13380a87f99a954e7d3bd06e79df243d6c1cd
Tim Lee <systeco@hotmail.com>
2022-08-29 09:25:06 -0400

    cart item quantity update

--- pages/cart.js
+++ pages/cart.js
@@ -12,6 +12,10 @@ export default function CartScreen() {
     const removeItemHandler = (item) => {
       dispatch({type: 'CART_REMOVE_ITEM', payload: item});
     };
+    const updateCartHandler = (item, qty) => {
+      const quantity = Number(qty);
+      dispatch({type: 'CART_ADD_ITEM', payload:{...item, quantity}})
+    }
   return (
     <Layout title='Shopping Cart'>
       <h1 className='mb-4 text-xl'>Shopping Cart</h1>
@@ -37,11 +41,17 @@ export default function CartScreen() {
                         <Link href={`/product/${item.slug}`}>
                           <a className='flex items-center'>
                             <Image src={item.image} alt={item.name} width={50} height={60} />
-                            &nbsp; {item.name}
+                            &nbsp; {item.name} &nbsp; {item.countInStock}
                           </a>
                         </Link>
                       </td>
-                      <td className='p-5 text-right'>{item.quantity}</td>
+                      <td className='p-5 text-right'>
+                          <select value={item.quantity} onChange={(e) => updateCartHandler(item, e.target.value)}>
+                              { [...Array(item.countInStock).keys()].map((x) => (
+                                <option key={x + 1} value={x + 1}>{x + 1}</option>
+                              ))}
+                          </select>  
+                      </td>
                       <td className='p-5 text-right'>{item.price}</td>
                       <td className='p-5 text-center'>
                         <button onClick={() => removeItemHandler(item)}>Delete</button>
