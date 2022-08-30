59c7dcb78fcc4b0fb730f78cec8e20fe86366976
Tim Lee <systeco@hotmail.com>
2022-08-30 17:25:54 -0400

    prodcut database insert

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,12 +1,16 @@
-import User from "../../models/Users";
-import data from '../../utils/data';
-import db from '../../utils/db';
+import db from "../../utils/db"
+import User from '../../models/Users';
+import data from '../../utils/data'
+import Product from "../../models/Product";
 
-const handler = async(req, res) => {
-    await db.connect();
-    await User.deleteMany();
-    await User.insertMany(data.users);
-    await db.disconnect();
-    res.send({message: 'seeded successfully'});
-};
+
+const handler = async (req, res) => {
+        await db.connect();
+        await User.deleteMany();
+        await User.insertMany(data.users);
+        await Product.deleteMany();
+        await Product.insertMany(data.products);
+        await db.disconnect();
+        res.send({ message: 'seeded successfully'})
+}
 export default handler;
