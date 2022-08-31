d3744caf5624dffd55de1ab7beb2d1951790dbee
Tim Lee <systeco@hotmail.com>
2022-08-31 17:01:46 -0400

    user registration page

--- pages/register.js
+++ pages/register.js
@@ -6,6 +6,7 @@ import {signIn, useSession} from 'next-auth/react';
 import {getError} from '../utils/error';
 import {toast} from 'react-toastify';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function LoginScreen() {
     const { data: session } = useSession();
@@ -21,11 +22,15 @@ export default function LoginScreen() {
     const {
         handleSubmit,
         register,
+        getValues,
         formState: { errors },
     } = useForm();
 
-    const submitHandler = async ({email, password}) => {
+    const submitHandler = async ({name, email, password}) => {
        try {
+        await axios.post('/api/auth/signup', {
+            name, email, password
+        })
          const result = await signIn('credentials', {
             redirect: false, email, password,
          });
@@ -37,33 +42,56 @@ export default function LoginScreen() {
         }
     }
       return (
-    <Layout title="Login">
+    <Layout title="Register">
         <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
-            <h1 className='mb-4 text-xl'>Login</h1>
+            <h1 className='mb-4 text-xl'>Register</h1>
+            <div className='mb-4'>
+                <label htmlFor='name'>Name</label>
+                <input type="name" {...register('name', {required: 'Please enter name', })} 
+                       className="w-full" id="name" autoFocus ></input>
+            {errors.name && (<div className='text-red-500'>{errors.name.message}</div>)}
+            </div>
             <div className='mb-4'>
                 <label htmlFor='email'>Email</label>
                 <input type="email" {...register('email', {required: 'Please enter email', 
                 pattern: {
                     value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z-_9]+.[a-zA-Z0-9-.]+$/i,                
                     message: 'Please enter valid email'
-                }})} className="w-full" id="email" autoFocus ></input>
+                }})} className="w-full" id="email" ></input>
+                 {errors.email && (<div className='text-red-500'>{errors.email.message}</div>)}
             </div>
-            {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+           
             <div className='mb-4'>
                 <label htmlFor='password'>Password</label>
                 <input type="password"  
                 {...register('password',{
                     required: 'Please enter password',
-                    minLength: {value: 6, message: "Password is more than 5 charts"},
-                })}
+                    minLength: {value: 6, message: "Password is more than 5 charts"},})}
                 className="w-full" id="password" autoFocus></input>
+                 {errors.password && (<div className='text-red-500'>{errors.password.message}</div>)}
             </div>
+           
+
+
             <div className='mb-4'>
-                <button className='primary-button'>Login</button>
+                <label htmlFor='confirmPassword'>confirm Password</label>
+                <input type="confirmPassword"  
+                {...register('confirmPassword',{
+                    required: 'Please enter confirmPassword',
+                    validate: (value) => value === getValues('password'),
+                    minLength: {value: 6, message: "confirmPassword is more than 5 charts"},})}
+                className="w-full" id="confirmPassword" autoFocus></input>
+                 {errors.confirmPassword && (<div className='text-red-500'>{errors.confirmPassword.message}</div>)}
+                 {errors.confirmPassword && errors.confirmPassword.type === 'validate' 
+                && (<div className='text-red=500'>Passsword does not match</div>)}
+            </div>
+           
+            <div className='mb-4'>
+                <button className='primary-button'>Register</button>
             </div>
             <div className='mb-4'>
                 Don&apos;t have an account? 
-                <Link href='./register'> Register </Link>
+                <Link href={`/register ? redirect=${redirect || '/'}`}> Register </Link>
             </div>
         </form>
     </Layout>
